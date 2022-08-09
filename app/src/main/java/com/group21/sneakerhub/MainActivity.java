1a18d04ca71b20d84f11997f01d0746e2a2ca5af
hqu118 <hqu118@aucklanduni.ac.nz>
2022-08-09 21:43:47 +1200

    firebase firestore setup

--- app/src/main/java/com/group21/sneakerhub/MainActivity.java
+++ app/src/main/java/com/group21/sneakerhub/MainActivity.java
@@ -1,8 +1,18 @@
 package com.group21.sneakerhub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("Products").document("1");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        System.out.println(document.getData()+" dawdawd");
+                    } else {
+                        System.out.println("not existsssss");
+                    }
+                } else {
+                    System.out.println("errrrroorororor"+task.getException());
+                }
+            }
+        });
+
     }
 }
