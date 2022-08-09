6f3eceb994e0bfe1de6ba868c7c1d1dad910d90e
hqu118 <hqu118@aucklanduni.ac.nz>
2022-08-09 22:43:05 +1200

    removed firebase connection testing code

--- app/src/main/java/com/group21/sneakerhub/MainActivity.java
+++ app/src/main/java/com/group21/sneakerhub/MainActivity.java
@@ -1,16 +1,8 @@
 package com.group21.sneakerhub;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,18 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference docRef = db.collection("products");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    System.out.println("result "+ task.getResult().getDocuments().get(0).getId() + task.getResult().getDocuments().get(0).get("name"));
-                } else {
-                    System.out.println("Error getting documents."+ task.getException());
-                }
-            }
-        });
-
     }
 }
