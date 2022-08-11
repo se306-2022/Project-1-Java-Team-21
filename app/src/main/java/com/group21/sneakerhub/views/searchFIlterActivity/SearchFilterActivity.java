335928382720178ab4ebec0bd11018abd9cb5c05
Jaskaran <jsan956@aucklanduni.ac.nz>
2022-08-12 02:53:13 +1200

    implemented the search bar and imageview for return button, as well as implemented search view listener to retrieve query string

--- app/src/main/java/com/group21/sneakerhub/views/searchFIlterActivity/SearchFilterActivity.java
+++ app/src/main/java/com/group21/sneakerhub/views/searchFIlterActivity/SearchFilterActivity.java
@@ -3,9 +3,14 @@ package com.group21.sneakerhub.views.searchFIlterActivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -44,5 +49,47 @@ public class SearchFilterActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.options_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) findViewById(R.id.menu_search);
+
+        searchView.setQueryHint("Search for sneaker...");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // when user hits return the final search string is returned
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                doMySearch(query);
+                return false;
+            }
+
+            // updates everytime a character changes in the searchbox
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                doMySearch(newText);
+                return false;
+            }
+        });
+
+
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
+    }
+
+    public void doMySearch(String query){
+        // correctly retrieves query to screen
+        System.out.println(query);
     }
 }
