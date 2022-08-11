8b3173bf794af4c5cf50f44f6d97909567d86da8
Jaskaran <jsan956@aucklanduni.ac.nz>
2022-08-12 00:31:05 +1200

    implemented nav bar

--- app/src/main/java/com/group21/sneakerhub/views/searchFIlterActivity/SearchFilterActivity.java
+++ app/src/main/java/com/group21/sneakerhub/views/searchFIlterActivity/SearchFilterActivity.java
@@ -1,10 +1,17 @@
 package com.group21.sneakerhub.views.searchFIlterActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.group21.sneakerhub.R;
+import com.group21.sneakerhub.views.favouriteActivity.FavouriteActivity;
+import com.group21.sneakerhub.views.mainActivity.MainActivity;
 
 public class SearchFilterActivity extends AppCompatActivity {
 
@@ -12,5 +19,30 @@ public class SearchFilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_filter);
+
+        // Initialize and assign object for nav bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.search);
+
+        // implement event listener
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.search:
+                        return true;
+                    case R.id.favourite:
+                        startActivity(new Intent(getApplicationContext(), FavouriteActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                }
+                return false;
+            }
+        });
     }
 }
