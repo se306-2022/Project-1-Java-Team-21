05c90503c4fa17c008bf402a0c6fa9b3b36407c0
iane056 <iane056@aucklanduni.ac.nz>
2022-08-12 15:50:36 +1200

    Populating firestore

--- app/src/main/java/com/group21/sneakerhub/views/mainActivity/MainActivity.java
+++ app/src/main/java/com/group21/sneakerhub/views/mainActivity/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.group21.sneakerhub.R;
+import com.group21.sneakerhub.model.Product;
 import com.group21.sneakerhub.views.favouriteActivity.FavouriteActivity;
 import com.group21.sneakerhub.views.searchFIlterActivity.SearchFilterActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addProductsToFireStore();
 
         // Initialize and assign object for nav bar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -47,5 +57,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+    // Add product documents to Firestore
+    public static List<Product> getProducts() {
+        List<Product> productsList = new ArrayList<Product>();
+        Map<Integer, String> name;
+        int id = key;
+        Product p = new Product();
+        return productsList;
+    }
+
+
+    public static void addProductsToFireStore() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        List<Product> productsList = getProducts();
+        for (Product p : productsList) {
+            db.collection("numbers").document("product " + productsList.set(p).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Log.d("Products Collection Add", "product " + getProducts() + " added.");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("Product Collection Add", "number " + getProducts() + " NOT added.");
+                }
+            }));
+        }
     }
 }
