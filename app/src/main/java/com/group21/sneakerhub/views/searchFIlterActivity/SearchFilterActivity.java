63a14af661ee5b07857beb3888c65af21a746ac8
hqu118 <hqu118@aucklanduni.ac.nz>
2022-08-21 23:45:49 +1200

    fixed animation

--- app/src/main/java/com/group21/sneakerhub/views/searchFIlterActivity/SearchFilterActivity.java
+++ app/src/main/java/com/group21/sneakerhub/views/searchFIlterActivity/SearchFilterActivity.java
@@ -263,30 +263,4 @@ public class SearchFilterActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent touchEvent){
-        switch(touchEvent.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                x1 = touchEvent.getX();
-                y1 = touchEvent.getY();
-                if (x1 < x2) {
-                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = touchEvent.getX();
-                y2 = touchEvent.getY();
-                if(x1 > x2){
-                    Intent intent = new Intent(getBaseContext(), FavouriteActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                }
-                break;
-        }
-        return false;
-
-    }
-
 }
